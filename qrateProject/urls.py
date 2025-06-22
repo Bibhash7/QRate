3902139e57496c4d2f649ec86e14b95bbe2ef5d2
Bibhash7 <tranceispermanent@gmail.com>
2025-06-23 02:02:24 +0530

    Fix:Fixed-urls-for-prod

--- qrateProject/urls.py
+++ qrateProject/urls.py
@@ -4,9 +4,17 @@ from django.urls import path, re_path
 from qrateApp import views
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', views.home, name='home'),
-    path('code/<slug:slug>/', views.view_code, name='view_code'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + [re_path(r'^.*$', views.handle_unknown_routes)]
+if settings.DEBUG:
+    urlpatterns = [
+        path("admin/", admin.site.urls),
+        path('', views.home, name='home'),
+        path('code/<slug:slug>/', views.view_code, name='view_code'),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+      + [re_path(r'^.*$', views.handle_unknown_routes)]
+else:
+    urlpatterns = [
+      path('', views.home, name='home'),
+      path('code/<slug:slug>/', views.view_code, name='view_code'),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+    + [re_path(r'^.*$', views.handle_unknown_routes)]
+
